7a1973a1cf661806cb1bd51a1c2f29ac0fd1c16b
csmai <maicsilla@gmail.com>
2023-10-13 10:52:57 +0200

    run locally

--- Code/02-Spark RDDs/Save_the_file.py
+++ Code/02-Spark RDDs/Save_the_file.py
@@ -1,18 +1,30 @@
 # Databricks notebook source
 from pyspark import SparkConf, SparkContext
 
-# Specify the local file path correctly
-file_path = "file:///C:/Users/User/Desktop/OneDrive/pylearn/course-master-big-data-with-pyspark-and-aws/Code/Sample.txt"
+# Specify the full path of Sample.txt
+file_path = (
+    "file:///home/csmai/course-master-big-data-with-pyspark-and-aws/Code/Sample.txt"
+)
+
 
 # Configure
 conf = SparkConf().setAppName("Read and write sample file")
 sc = SparkContext.getOrCreate(conf=conf)
 rdd = sc.textFile(file_path)
+rdd = rdd.repartition(5)
+rdd2 = rdd.flatMap(lambda line: line.split())
 
 # Commands
-print(rdd.collect())
+print(rdd2.collect())
+print(rdd2.getNumPartitions())
 
+rdd2 = rdd2.coalesce(3)
 # Specify the output directory
-output_dir = "file:///C:/Users/User/Desktop/OneDrive/pylearn/course-master-big-data-with-pyspark-and-aws/Code/output/saved_sample"
+output_dir = "file:///home/csmai/course-master-big-data-with-pyspark-and-aws/Code/output/partition_test3"
 # Save
-rdd.flatMap(lambda line: line.split()).saveAsTextFile(output_dir)
+rdd2.saveAsTextFile(output_dir)
+
+rdd_read = sc.textFile(output_dir)
+# Commands
+print(rdd_read.collect())
+print(rdd_read.getNumPartitions())
